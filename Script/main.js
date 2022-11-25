2e0f4ed5b901ae04f3c6b8250367f209e19bbd77
asifaminisonline <asifaminisonline@gmail.com>
2022-11-25 15:17:01 +0530

    projected completed

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,4 @@
+// Hamburger menu
 const script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
@@ -10,4 +11,62 @@ menuBtn.addEventListener('click', () => {
   $('.mobile-nav ul li a').on('click', () => {
     $('#onclick').click();
   });
+});
+// end of Hamburger menu
+const ourTeam = [
+  {
+    image: '/Capstone-Project/assets/1.png',
+    title: 'Scott Charles',
+    subTitle: 'Software Engineer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  },
+  {
+    image: '/Capstone-Project/assets/2.webp',
+    title: 'Scott Charles',
+    subTitle: 'Frontend Developer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  }, {
+    image: '/Capstone-Project/assets/3.jpg',
+    title: 'Scott Charles',
+    subTitle: 'Backend Developer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  }, {
+    image: '/Capstone-Project/assets/4.jpg',
+    title: 'Scott Charles',
+    subTitle: 'Full Stack Developer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  }, {
+    image: '/Capstone-Project/assets/5.jpeg',
+    title: 'Scott Charles',
+    subTitle: 'DevOps Engineer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  }, {
+    image: '/Capstone-Project/assets/6.jpeg',
+    title: 'Scott Charles',
+    subTitle: 'Software Engineer',
+    info: 'Worked with other programmers to develop Kiyu the best online tranding platfom in 2017.',
+  },
+
+];
+
+ourTeam.forEach((element, i) => {
+  const team = document.querySelector('#team');
+  console.log(team);
+  const card = document.createElement('div');
+  card.classList = 'card';
+  const teamcard = `
+  <div class="row" id="dynamic-card">
+ <div class="col-sm-12 col-md-12" id="hor">
+  <img src="${ourTeam[i].image}">
+  <div class="details">
+    <h4 class="CT">${ourTeam[i].title}</h4>
+    <h6 class="CST">${ourTeam[i].subTitle}</h6>
+    <p class="INFO">${ourTeam[i].info}</p>
+  </div>
+ </div>
+ </div>
+
+`;
+  card.innerHTML = teamcard;
+  team.appendChild(card);
 });
